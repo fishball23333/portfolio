2fb00728a49e3820a3a23a0f40972a5c18aabb57
fishball <markyu132@gmail.com>
2023-04-24 11:21:39 -0400

    4-14

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -23,19 +23,6 @@ const Projects = () => {
       <tbody>
         <tr>
           <td className='align-middle'>1</td>
-          <td className='align-middle'>Home-meal website </td>
-          <td>
-            <p>The data of food menu and user accounts are fetched from personal database from MongoDB</p>
-            <p>A session with special authorities for a logged-in customer or a clerk can be hosted</p>
-            <p>User can receive an email whenever making an order, making a registration, by using SendGrid</p>
-          </td>
-          <td className='align-middle'>
-            <a href = "http://web-demo.herokuapp.com/">http://web-demo.herokuapp.com/</a>
-          </td>
-        </tr>
-
-        <tr>
-          <td className='align-middle'>2</td>
           <td className='align-middle'>AWS microservices  </td>
           <td>
             <p>Developed a web hosted on ECS that can interact with different AWS services including DynamoDB and S3 for maintaining uploaded user files in different data types </p>
