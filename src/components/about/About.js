82d39bfe8dfd5651ac1c36087dfeef4142972ab7
fishball <markyu132@gmail.com>
2023-05-12 10:42:30 -0400

    update

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -16,7 +16,7 @@ const About = () => {
         </Col>
         <Col md={6} sm={12} >        
           <h1>About</h1>
-          <h4>Hi, my name is Binwei Yu, currently a 3rd year Seneca student studying Computer Programming & Analysis. </h4>
+          <h4>Hi, my name is Binwei Yu, a new graduate of Computer Programming program in Seneca College. </h4>
           <h4>In my previous two year study period, I have explored and mastered many programming languages and skills, and those skills can be found in the specific section in the page below. </h4>
           <h4>I have strong desire and passion for improving and learning more on programming-related skills, and I'm really addicted to coding throughout my recent two years of study period and I love solving programming puzzles all day long in front of my computer. </h4>
           <h4>I'm looking forward to becoming a professional software developer, and I believe my hard work in this field will get paid off one day, and I will be the useful contributor for the company if I have a chance to get hired. </h4>
