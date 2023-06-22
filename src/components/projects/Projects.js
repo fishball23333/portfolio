37bbbdb313e378727928c2be0cf5486089e52ca5
fishball <markyu132@gmail.com>
2023-06-21 22:49:16 -0400

    kanban

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -27,7 +27,7 @@ const Projects = () => {
           <td>
             <p> •	Implemented unit test, integration test and CI/CD pipeline throughout software development life cycle Utilized multiple AWS services (e.g. Cognito, EC2, ECS, DynamoDB, S3) and microservices architecture </p>
             <p> •	Handled users authorization and authentication </p>
-            <p> •	Developed REST APIs for users to add/read/update/delete large files to databwases</p>
+            <p> •	Developed REST APIs for users to add/read/update/delete large files to databases</p>
             <p> •	Allowed users to convert specific file types to other supported types </p>
             <p> •	Implemented unit test, integration test and CI/CD pipeline throughout software development life cycle</p>
 
@@ -48,6 +48,18 @@ const Projects = () => {
           </td>
           <td className='align-middle'>
             Currently unavailable due to AWS student account is expired
+          </td>
+        </tr>
+
+        <tr>
+          <td className='align-middle'>3</td>
+          <td className='align-middle'>Kanban Board</td>
+          <td>
+             <p> • Implemented an simple kanban board web app based on vite which provides the functionality to drag an order to the different stage </p>
+             <p> • Allowed users to order and check history items from the local database served by "JSON server" </p>
+          </td>
+          <td className='align-middle'>
+            repository link: <a href="https://github.com/fishball23333/viteLearning">https://github.com/fishball23333/viteLearning</a>
           </td>
         </tr>
       </tbody>
